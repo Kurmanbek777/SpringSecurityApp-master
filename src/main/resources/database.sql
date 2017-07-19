f75bc25e8cf89a11eb42772c9cc9a77f7a84d2b4
Kurmanbek777 <knurkamil@gmail.com>
2017-07-19 21:00:09 +0000

    авторизация spring secuirity
    авторизация spring secuirity

---
+++ src/main/resources/database.sql
@@ -0,0 +1,39 @@
+-- Table: users
+CREATE SEQUENCE users_seq;
+
+CREATE TABLE users (
+  id       INT          NOT NULL DEFAULT NEXTVAL ('users_seq') PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+)
+ ;
+
+-- Table: roles
+CREATE SEQUENCE roles_seq;
+
+CREATE TABLE roles (
+  id   INT          NOT NULL DEFAULT NEXTVAL ('roles_seq') PRIMARY KEY,
+  name VARCHAR(100) NOT NULL
+)
+ ;
+
+-- Table for mapping user and roles: user_roles
+CREATE TABLE user_roles (
+  user_id INT NOT NULL,
+  role_id INT NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (role_id) REFERENCES roles (id),
+
+  UNIQUE (user_id, role_id)
+)
+ ;
+
+-- Insert data
+
+INSERT INTO users VALUES (3, 'proselyte', '$2a$11$uSXS6rLJ91WjgOHhEGDx..VGs7MkKZV68Lv5r1uwFu7HgtRn3dcXG');
+
+INSERT INTO roles VALUES (1, 'ROLE_USER');
+INSERT INTO roles VALUES (2, 'ROLE_ADMIN');
+
+INSERT INTO user_roles VALUES (1, 2);
